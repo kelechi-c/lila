84450f87827d61732d2009ab6f080e28eb86281d
Tensor <narutokelechi@gmail.com>
2024-08-13 14:51:31 +0100

    [modify] add patchify layer and basic ViT template

--- ViT/model_blocks.py
+++ ViT/model_blocks.py
@@ -1,8 +1,39 @@
 import torch
 from torch import nn
-from timm.layers import LayerNorm
+from ViT.utils import config
+from timm.layers import PatchEmbed
+from einops import rearrange
+from einops.layers.torch import Rearrange
 
 
-class PatchEmbed(nn.Module):
-    def __init__(self) -> None:
-        super().__init__(*args, **kwargs)
+class Patchify(nn.Module):
+    def __init__(self, patches=16):
+        super().__init__()
+        self.patch_size = patches
+        self.patch_module = nn.Unfold(kernel_size=patches, stride=patches)
+
+    def forward(self, x: torch.Tensor):
+        batch_s, ch, _, _ = x.shape
+
+        x = self.patch_module(x)
+        x = x.view(batch_s, ch, self.patch_size, self.patch_size, -1)
+        x = x.permute(0, 4, 1, 2, 3)
+
+        return x
+
+
+class ViTBlock(nn.Module):
+    def __init__(self, img_size, classes, embed_dim, in_channels, config=config):
+        super().__init__()
+        self.patchlayer = PatchEmbed()
+        self.mlp_layer = nn.Sequential(
+            nn.Linear(768, 1024), nn.GELU(), nn.Linear(1024, 768)
+        )
+
+        self.cls_token = nn.Parameter(torch.ones(1, 1, embed_dim))
+
+    def forward(self, x):
+        img_patches = self.patchlayer(x)
+        x = self.mlp_layer(img_patches)
+
+        return x
---
+++ ViT/utils.py
@@ -0,0 +1,24 @@
+import torch
+import cv2
+import numpy as np
+
+
+class config:
+    lr = 1e-4
+    image_size = 224
+    batch_size = 32
+    embed_dim = 768
+    patch_size = 16
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    dataset_id = ""
+    split = 5000
+    dtype = torch.float32
+
+
+def read_image(img):
+    img = np.array(img)
+    img = cv2.resize(img, (config.image_size, config.image_size))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img / 255.0
+
+    return img
