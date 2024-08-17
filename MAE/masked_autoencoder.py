20699dd287eede036f74ffe35a4d152c8637b437
Tensor <narutokelechi@gmail.com>
2024-08-17 12:37:33 +0100

    [add] decoder/encoder block structure/configs

--- MAE/masked_autoencoder.py
+++ MAE/masked_autoencoder.py
@@ -1,6 +1,6 @@
 import torch
-from torch import isin, nn
-from timm.models.vision_transformer import PatchEmbed
+from torch import nn
+from timm.models.vision_transformer import Block, PatchEmbed
 
 
 class MaskedAutoencoder(nn.Module):
@@ -12,38 +12,78 @@ class MaskedAutoencoder(nn.Module):
         patch_size=16,
         decoder_dim=512,
         decoder_depth=8,
+        num_heads=16,
+        mlp_ratio=4,
+        depth=12,
     ):
         self.patch_embed = PatchEmbed(
             img_size, patch_size, in_channels, embed_dim
         )  # initialize patchembed layer with custom configs
 
-        self.layer_norm = nn.LayerNorm(embed_dim)
-
         # encoder section blocks
 
+        self.layer_norm = nn.LayerNorm(embed_dim)
         num_patches = self.patch_embed.num_patches
 
-        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.cls_token = nn.Parameter(
+            torch.zeros(1, 1, embed_dim)
+        )  # encoder class token
         self.pos_embed = nn.Parameter(
-            torch.zeros(1, num_patches + 1, embed_dim), requires_grad=False
+            torch.zeros(1, num_patches + 1, embed_dim),
+            requires_grad=False,  # fixed sin-cos embedding
         )
-        self._pos_embed.requires_grad = False
 
-        self.encoder_layers = nn.Sequential()
+        self.encoder_layers = nn.ModuleList(
+            [
+                Block(
+                    embed_dim,
+                    num_heads,
+                    mlp_ratio,
+                    qkv_bias=True,
+                    norm_layer=self.layer_norm,
+                )
+                for _ in range(depth)
+            ]
+        )
 
+        #### ______#####
         # decoder section/blocks
-        self.mask_token = nn.Parameter(  # # define mask tokens
+        self.mask_token = nn.Parameter(  # define mask tokens
             torch.zeros(1, 1, decoder_dim)
         )
         self.decoder_pos_embed = nn.Parameter(
             torch.zeros(1, num_patches + 1, decoder_dim), requires_grad=False
         )
-        self.decoder_norm = nn.LayerNorm(decoder_dim)
 
-    def _init_weights(self, ml):
+        self.decoder_norm = nn.LayerNorm(decoder_dim)
+        self.decoder_embedding = nn.Linear(
+            embed_dim, decoder_dim, bias=True
+        )  # map to decoder dimensionality
+
+        self.decoder_layers = nn.ModuleList(  # blocks for shallow MAE decoder
+            [
+                Block(
+                    decoder_dim,
+                    num_heads,
+                    mlp_ratio,
+                    qkv_bias=True,
+                    norm_layer=self.decoder_norm,
+                )
+                for _ in range(decoder_depth)
+            ]
+        )
+
+        self.output_layer = nn.Linear(
+            decoder_dim, (patch_size**2) * in_channels, bias=True
+        )  # output/prediction layer
+
+    def _init_weights(self, ml):  # instantiate weights for seleccted layers/nn
         if isinstance(ml, nn.Linear):
             nn.init.xavier_uniform_(ml.weight)
 
         if isinstance(ml, nn.LayerNorm):
             nn.init.constant_(ml.weight, 0)
             nn.init.constant_(ml.bias, 0)
+
+    def forward(self, x: torch.Tensor):
+        return x
